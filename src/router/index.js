e96a6b255f0ef76ef38fea8a5bfe69d89881a3f1
githubflower <v5baqi@yeah.net>
2018-04-28 01:28:21 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import BusinessOrder from '../pages/BusinessOrder.vue'
 Vue.use(Router)
 
 export default new Router({
-	mode: 'history',
+	// mode: 'history',
 	routes: [{
 		path: '/',
 		name: 'BusinessOrder',
