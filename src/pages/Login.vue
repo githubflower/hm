9668ec4784ed3ea094d5944cc9bc10c39eb5ce47
zjie <v5baqi@yeah.net>
2018-05-17 18:01:19 +0800

    add login module

---
+++ src/pages/Login.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="login-wrap">
+		<el-form ref="form" :model="form" label-width="80px">
+			<el-form-item label="用户名">
+                <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="form.password" type="password"></el-input>
+            </el-form-item>
+		</el-form>
+        <div class="btn-wrap">
+            <el-button @click="login">登录</el-button>
+            <!-- <el-button @click="regist">注册</el-button> -->
+        </div>
+	</div>
+</template>
+<script>
+    export default{
+        name: 'Login',
+        data(){
+            return {
+                form: {
+                    name: '',
+                    password: ''
+                }
+            }
+        },
+        methods: {
+            login(){}/*,
+            regist(){
+                this.$router.push('/regist');
+            }*/
+        }
+    }
+</script>
+<style>
+    .login-wrap{
+        margin: 80px auto;
+        width: 300px;
+        text-align: center;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import BusinessOrder from '../pages/BusinessOrder.vue'
 import ProxyOrder from '../pages/ProxyOrder.vue'
+import Login from '../pages/Login.vue'
 import TestPage from '../pages/TestPage.vue'
 
 Vue.use(Router)
@@ -19,6 +20,10 @@ export default new Router({
 		name: 'ProxyOrder',
 		component: ProxyOrder
 	},{
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },{
 		path: '/test',
 		name: 'TestPage',
 		component: TestPage
